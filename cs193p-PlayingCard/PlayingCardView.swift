6b239d9c680171250fd3c1faef5a29eb197e3fe1
Grigoriy Romanyuk <skorovodkin@icloud.com>
2019-03-14 11:07:15 -0700

    Clear drawings. Rounded corners.

--- cs193p-PlayingCard/PlayingCardView.swift
+++ cs193p-PlayingCard/PlayingCardView.swift
@@ -13,22 +13,10 @@ class PlayingCardView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-//        if let context = UIGraphicsGetCurrentContext() {
-//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0.0, endAngle: 2.0*CGFloat.pi, clockwise: true)
-//            context.setLineWidth(5.0)
-//            UIColor.green.setFill()
-//            UIColor.red.setStroke()
-//            context.strokePath()
-//            context.fillPath()
-//        }
-        
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0.0, endAngle: 2.0*CGFloat.pi, clockwise: true)
-        path.lineWidth = 5.0
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.stroke()
-        path.fill()
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
     }
 
 }
