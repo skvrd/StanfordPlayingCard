9c38c269344e769290c4a9519ff7951d65b53143
Grigoriy Romanyuk <skorovodkin@icloud.com>
2019-03-14 16:34:54 -0700

    Make view configurable through the interface builder.

--- cs193p-PlayingCard/PlayingCardView.swift
+++ cs193p-PlayingCard/PlayingCardView.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
+@IBDesignable
 class PlayingCardView: UIView {
 
-    var rank: Int = 6 {didSet { setNeedsDisplay(); setNeedsLayout() }}
+    @IBInspectable
+    var rank: Int = 12 {didSet { setNeedsDisplay(); setNeedsLayout() }}
+    
+    @IBInspectable
     var suit: String = "♥️" {didSet { setNeedsDisplay(); setNeedsLayout() }}
-    var isFaceUp: Bool = false {didSet { setNeedsDisplay(); setNeedsLayout() }}
+    
+    @IBInspectable
+    var isFaceUp: Bool = true {didSet { setNeedsDisplay(); setNeedsLayout() }}
     
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
@@ -110,7 +116,7 @@ class PlayingCardView: UIView {
         UIColor.white.setFill()
         roundedRect.fill()
         if isFaceUp {
-            if let faceCardImage = UIImage(named: rankString + suit) {
+            if let faceCardImage = UIImage(named: rankString + suit, in:Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
                 faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
             } else {
                 drawPips()
