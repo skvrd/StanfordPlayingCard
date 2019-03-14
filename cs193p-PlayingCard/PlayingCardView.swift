4954534004a1eb8846416b5b64e2c2f5a1c7dd2c
Grigoriy Romanyuk <skorovodkin@icloud.com>
2019-03-14 16:55:46 -0700

    Add gestures. tap pinch swipe

--- cs193p-PlayingCard/PlayingCardView.swift
+++ cs193p-PlayingCard/PlayingCardView.swift
@@ -20,6 +20,17 @@ class PlayingCardView: UIView {
     @IBInspectable
     var isFaceUp: Bool = true {didSet { setNeedsDisplay(); setNeedsLayout() }}
     
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { setNeedsDisplay() } }
+    
+    @objc func adjustFaceCardScale(byHandlingGestureRecognizedBy recognizer: UIPinchGestureRecognizer) {
+        switch recognizer.state {
+        case .changed, .ended:
+            faceCardScale *= recognizer.scale
+            recognizer.scale = 1.0
+        default: break
+        }
+    }
+    
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
         font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
@@ -117,7 +128,7 @@ class PlayingCardView: UIView {
         roundedRect.fill()
         if isFaceUp {
             if let faceCardImage = UIImage(named: rankString + suit, in:Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
-                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
             } else {
                 drawPips()
             }
--- cs193p-PlayingCard/ViewController.swift
+++ cs193p-PlayingCard/ViewController.swift
@@ -12,16 +12,37 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        for _ in 1...3 {
-            if let card = deck.draw() {
-                print(card)
-            }
+
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
         }
     }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
-
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch  sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default: break
+        }
+        
+    }
+    
 }
 
