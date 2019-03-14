6abc85dc16247e977b99ee331a9a4210d14ce4cf
Grigoriy Romanyuk <skorovodkin@icloud.com>
2019-03-14 10:50:09 -0700

    Playing around with Draw

---
+++ cs193p-PlayingCard/PlayingCardView.swift
@@ -0,0 +1,34 @@
+//
+//  PlayingCardView.swift
+//  cs193p-PlayingCard
+//
+//  Created by Grigoriy Romanyuk on 3/14/19.
+//  Copyright © 2019 Grigoriy Romanyuk. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+//        if let context = UIGraphicsGetCurrentContext() {
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0.0, endAngle: 2.0*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            context.strokePath()
+//            context.fillPath()
+//        }
+        
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0.0, endAngle: 2.0*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
+    }
+
+}
